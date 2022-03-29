b90d5ff3a5adceefa744fcb79776e895b4c30a74
Web Source (SRC) <web.system.management@gmail.com>
2022-03-29 11:40:28 +0300

    Update get_real_data_PM.py

--- get_real_data_PM.py
+++ get_real_data_PM.py
@@ -7,19 +7,20 @@ from obspy import read
 client = Client("GFZ")
 
 t1 = UTCDateTime.now()
+t1.isoformat()
 
 start = t1 - 60*60*12
 endtime = start + 60*60*12
 
 try:
- wave1 = client.get_waveforms('PM', 'ROSA', '--', 'BHZ', start.replace(minute=0, second=1), endtime)
+ wave1 = client.get_waveforms('PM', 'ROSA', '--', 'BHZ', start.replace(minute=0, second=0), endtime)
  wave1.filter('highpass', freq=1, corners=2, zerophase=True)
  #wave1.filter("bandpass", freqmin=0.2, freqmax=20, corners=3, zerophase=True)
  wave1.detrend(type='demean')
  wave1.detrend(type='linear')
- wave1.plot(type="dayplot", interval=30, size=(1360, 800), dpi=164, number_of_ticks=6, tick_rotation=45,
-        right_vertical_labels=True,
-        vertical_scaling_range=6e3, one_tick_per_line=True,
+ wave1.plot(type="dayplot", interval=30, title=str(wave1[0].id) + ' ' + str(wave1[0].stats.endtime.replace(microsecond=0).isoformat()), size=(1360, 800), dpi=164, number_of_ticks=6,
+        right_vertical_labels=False,
+        vertical_scaling_range=1000, one_tick_per_line=True,
         color=['k', 'r', 'b', 'g'], show_y_UTC_label=True,
         outfile='PM/ROSA/PM_12H_ROSA_BHZ.png')
 except Exception as e:
